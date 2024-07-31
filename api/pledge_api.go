59359cceefd54c84fc4fd2234f66ac9b61a8763c
Nick2022 <876927113@qq.com>
2024-07-31 14:24:41 +0800

    feat: use gin for http service.

--- api/pledge_api.go
+++ api/pledge_api.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"pledge-backend-practise/config"
 	"pledge-backend-practise/log"
 )
@@ -8,5 +10,32 @@ import (
 func main() {
 	log.Logger.Info("pledge api")
 
+	//init mysql
+
+	//init redis
+
+	//gin bind go-playground-validator
+
+	// websocket server
+
+	// get plgr price from kucoin-exchange
+
+	// gin start
+	gin.SetMode(gin.DebugMode)
+	app := gin.Default()
+	defer func(app *gin.Engine, port string) {
+		err := app.Run(port)
+		if err != nil {
+			log.Logger.Error("app run error", zap.Error(err))
+		}
+	}(app, ":"+config.Config.Env.Port)
 	log.Logger.Info(config.Config.Env.Port)
+
+	//app.Static("/storage/", staticPath)
+	//app.Use(middlewares.Cors()) // 「 Cross domain Middleware 」
+	//routes.InitRoute(app)
+	app.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{})
+	})
+
 }
